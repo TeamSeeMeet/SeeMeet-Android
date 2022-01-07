cb9c8374ceaa5c5f60d57bf6f26a57af1f70ee33
You-jeong136 <smj06168@gmail.com>
2022-01-07 16:38:02 +0900

    [FEAT] 홈_마이페이지 틀 구현

--- app/src/main/java/org/seemeet/seemeet/HomeFragment.kt
+++ app/src/main/java/org/seemeet/seemeet/HomeFragment.kt
@@ -1,9 +1,12 @@
 package org.seemeet.seemeet
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import org.seemeet.seemeet.databinding.FragmentHomeBinding
 
@@ -22,11 +25,31 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initClickListener()
+        initNaviDrawer()
     }
 
     private fun initClickListener(){
         binding.btnFriend.setOnClickListener {
             FriendActivity.start(requireContext())
         }
+
+        binding.btnMypageOpen.setOnClickListener{
+            binding.dlHomeMypage.openDrawer(GravityCompat.START)
+        }
+        binding.nvMypage.ivMypageBack.setOnClickListener {
+            binding.dlHomeMypage.closeDrawer(GravityCompat.START)
+        }
     }
+
+    private fun initNaviDrawer(){
+        val toggle = ActionBarDrawerToggle(
+            this.activity, binding.dlHomeMypage, R.string.home_drawer_open, R.string.home_drawer_close
+        )
+
+        binding.dlHomeMypage.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+    }
+
 }
