81811ad55f7c5e753d4ce836e8ac0e4118d90310
yulim <yulim13sky@naver.com>
2022-01-08 03:16:59 +0900

    [FEAT] 로그인->회원가입 연결

--- app/src/main/java/org/seemeet/seemeet/ui/registration/LoginActivity.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/registration/LoginActivity.kt
@@ -1,7 +1,8 @@
 package org.seemeet.seemeet.ui.registration
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import org.seemeet.seemeet.MainActivity
 import org.seemeet.seemeet.databinding.ActivityLoginBinding
 
@@ -9,11 +10,24 @@ class LoginActivity : AppCompatActivity() {
     private val binding: ActivityLoginBinding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        LoginBtn()
+        RegisterBtn()
+    }
+
+    fun LoginBtn() {
         binding.btnLogin.setOnClickListener {
             MainActivity.start(this)
+        }
+    }
+
+    fun RegisterBtn() {
+        binding.tvRegister.setOnClickListener {
+            val nextIntent = Intent(this, RegisterActivity::class.java)
+            startActivity(nextIntent)
         }
     }
 }
