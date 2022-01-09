c0540ec299037f893a4d27a4d19cc41f39727d31
Hyuna Kim <akxmcse@gmail.com>
2022-01-09 16:02:03 +0900

    [MOD] 친구 추가 틀 구현

---
+++ app/src/main/java/org/seemeet/seemeet/ui/friend/AddFriendActivity.kt
@@ -0,0 +1,12 @@
+package org.seemeet.seemeet.ui.friend
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.seemeet.seemeet.R
+
+class AddFriendActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_friend)
+    }
+}
