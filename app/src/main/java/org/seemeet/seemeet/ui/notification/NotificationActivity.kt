7109e75e9c1cb1dd9c2d57e3cc889071f43abe86
You-jeong136 <smj06168@gmail.com>
2022-01-11 12:33:53 +0900

    [FEAT] noti _ 탭 레이아웃 + 뷰페이저 추가.

--- app/src/main/java/org/seemeet/seemeet/ui/notification/NotificationActivity.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/notification/NotificationActivity.kt
@@ -4,13 +4,58 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.tabs.TabLayoutMediator
 import org.seemeet.seemeet.R
+import org.seemeet.seemeet.databinding.ActivityNotificationBinding
+import org.seemeet.seemeet.ui.notification.adapter.PagerFragmentStateAdapter
+import org.seemeet.seemeet.ui.notification.done.NotiDoneFragment
+import org.seemeet.seemeet.ui.notification.inprogress.NotiInProgressFragment
 
 class NotificationActivity : AppCompatActivity() {
+    private var _binding : ActivityNotificationBinding? = null
+    val binding get() = _binding!!
+
+    private val notiInProgressFragment by lazy { NotiInProgressFragment() }
+    private val notiDoneFragment by lazy { NotiDoneFragment() }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notification)
+       _binding = ActivityNotificationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initTab()
+        initClickListener()
     }
+
+
+    private fun initTab() {
+        val pagerAdapter = PagerFragmentStateAdapter(this)
+        pagerAdapter.addFragment(NotiInProgressFragment())
+        pagerAdapter.addFragment(NotiDoneFragment())
+
+        binding.vpNoti.adapter = pagerAdapter
+
+        TabLayoutMediator(binding.tlNoti, binding.vpNoti) { tab, position ->
+
+            when (position) {
+                0 -> {
+                    tab.text = getString(R.string.noti_inprogress)
+                }
+                1 -> {
+                    tab.text = getString(R.string.noti_done)
+                }
+            }
+        }.attach()
+    }
+    private fun initClickListener(){
+        binding.ivNotiTopBack.setOnClickListener {
+            Log.d("************notiback", "press back")
+        }
+    }
+
+
 
     companion object {
         fun start(context: Context) {
