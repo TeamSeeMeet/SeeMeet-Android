2855f12d8d020e922045713e0b2be2e947b3bf96
Hyuna Kim <akxmcse@gmail.com>
2022-01-14 01:02:33 +0900

    [MOD] 텍스트뷰 영역 수정에 따른 마진값 일부 수정

--- app/src/main/java/org/seemeet/seemeet/util/BindingAdapters.kt
+++ app/src/main/java/org/seemeet/seemeet/util/BindingAdapters.kt
@@ -1,26 +1,12 @@
 package org.seemeet.seemeet.util
 
-import android.graphics.Color
 import android.widget.ImageView
-import androidx.annotation.ColorInt
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.RecyclerView
 
 object BindingAdapters {
     @BindingAdapter("setSrc")
     @JvmStatic
     fun setSrc(imageView: ImageView, imgId: Int) {
         imageView.setImageResource(imgId)
-    }
-
-    @BindingAdapter( value = ["dividerHeight", "dividerPadding", "dividerColor"], requireAll = false )
-    @JvmStatic
-    fun RecyclerView.setDivider(dividerHeight: Float?, dividerPadding: Float?, @ColorInt dividerColor: Int?) {
-        val decoration = CustomDecoration(
-            height = dividerHeight ?: 0f,
-            padding = dividerPadding ?: 0f,
-            color = dividerColor ?: Color.TRANSPARENT
-        )
-        addItemDecoration(decoration)
     }
 }
