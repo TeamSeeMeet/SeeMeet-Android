c1e370b3a6e2f703abba2b60c510ae195af49172
Hyuna Kim <akxmcse@gmail.com>
2022-01-16 02:26:10 +0900

    [MOD] 친구 목록 뷰 디자인 수정

---
+++ app/src/main/java/org/seemeet/seemeet/ui/viewmodel/FriendViewModel.kt
@@ -0,0 +1,35 @@
+package org.seemeet.seemeet.ui.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import org.seemeet.seemeet.R
+import org.seemeet.seemeet.data.local.ReminderData
+
+class FriendViewModel(application: Application) : AndroidViewModel(application) {
+
+    //리싸이클러뷰에 들어갈 리스트 변수
+    private val _reminderList = MutableLiveData<List<ReminderData>>()
+    val reminderList : LiveData<List<ReminderData>>
+        get() = _reminderList
+
+
+    //임시로 넣을 더미데이터 셋팅. < 위의 리스트에 대입
+    fun setReminderList() {
+        _reminderList.value = mutableListOf(
+            ReminderData(
+                "대방어데이", R.drawable.circle_black, "1월 15일", "D-4"
+            ),
+            ReminderData(
+                "대방어데이2", R.drawable.circle_black, "1월 16일", "D-5"
+            ),
+            ReminderData(
+                "대방어데이3", R.drawable.circle_black, "1월 17일", "D-6"
+            )
+
+        )
+    }
+
+
+}
