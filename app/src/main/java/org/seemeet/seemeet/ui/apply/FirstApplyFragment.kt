4c348b06f41dc79f32afaf5d18893d476c2e2f3a
최유림 <chzh1323@gmail.com>
2022-01-17 06:33:03 +0900

    [FEAT] 칩그룹 close img 변경, recyclerAdapter 연결, 아이템 클릭 시 칩 생성, 친구 선택 후 키보드 완료 누르면 키보드 창 내려가고 리싸이클러뷰 사라짐 (친구 선택 시 back 버튼 동작 수정, 약속 제목 x 버튼, 다음 버튼 버그 수정 필요)

--- app/src/main/java/org/seemeet/seemeet/ui/apply/FirstApplyFragment.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/apply/FirstApplyFragment.kt
@@ -5,26 +5,23 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import androidx.activity.OnBackPressedCallback
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.chip.Chip
 import org.seemeet.seemeet.R
+import org.seemeet.seemeet.data.local.ApplyFriendData
 import org.seemeet.seemeet.databinding.FragmentFirstApplyBinding
-import java.util.*
+import org.seemeet.seemeet.ui.apply.adapter.ApplyFriendAdapter
 
 class FirstApplyFragment : Fragment() {
 
+    private lateinit var callback: OnBackPressedCallback
+
     private var _binding: FragmentFirstApplyBinding? = null
     val binding get() = _binding!!
-/*
-    private var friendlist // 데이터를 넣은 리스트변수
-            : MutableList<ApplyFriendData>? = null
- */
-
-    private var friendlist // 데이터를 넣은 리스트변수
-            : MutableList<String>? = null
+    private lateinit var adapter: ApplyFriendAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,101 +49,66 @@ class FirstApplyFragment : Fragment() {
     }
 
     private fun initAutoCompletetv() {
-        // 리스트를 생성한다.
-        friendlist = ArrayList()
 
-        // 리스트에 검색될 데이터(단어)를 추가한다.
-        settingList()
+        adapter = ApplyFriendAdapter()
 
-        val autoCompleteTextView =
-            binding.autoEtToWho
+        binding.rvFriend.adapter = adapter
 
-        // AutoCompleteTextView 에 어댑터를 연결한다.
-/*
-        var adapter = activity?.let {
-            ApplyFriendAdapter(
-                it,
-                friendlist as List<ApplyFriendData>
+        adapter.applyfriendList.addAll(
+            listOf(
+                ApplyFriendData(R.drawable.ic_btn_remove, "강유나"),
+                ApplyFriendData(R.drawable.ic_btn_remove, "나지현"),
+                ApplyFriendData(R.drawable.ic_btn_remove, "도승윤"),
+                ApplyFriendData(R.drawable.ic_btn_remove, "박나은"),
+                ApplyFriendData(R.drawable.ic_btn_remove, "박주혁"),
+                ApplyFriendData(R.drawable.ic_btn_remove, "박예림"),
+                ApplyFriendData(R.drawable.ic_btn_remove, "박한빈"),
+                ApplyFriendData(R.drawable.ic_btn_remove, "박수현"),
+                ApplyFriendData(R.drawable.ic_btn_remove, "김주현"),
+                ApplyFriendData(R.drawable.ic_btn_remove, "최유현")
             )
-        }
-*/
+        )
+        adapter.notifyDataSetChanged()
 
-        var adapter = activity?.let {
-            ArrayAdapter(
-                it, R.layout.item_apply_search_friend, R.id.tv_apply_name,
-                friendlist as List<String>
-            )
-        }
-
-
-/*
-        //커스텀 어댑터 생성하기
-        var adapter = activity?.let{
-            AutoCompleteAdapter(
-                it, R.layout.item_apply_search_friend, R.id.tv_apply_name,
-                friendlist as List<ApplyFriendData>
-            )
+        //키보드에서 완료 버튼 누르면
+        binding.etToWho.setOnEditorActionListener { textView, i, keyEvent ->
+            binding.tvWho.visibility = View.VISIBLE
+            binding.etToWho.clearFocus()
+            binding.rvFriend.visibility = View.INVISIBLE
+            false
         }
 
-*/
-        autoCompleteTextView.setAdapter(adapter)
-
-        autoCompleteTextView.setOnItemClickListener { parent, view, position, id ->
-
-            val string = binding.autoEtToWho.text
-            if (!string.isNullOrEmpty()) {
-                binding.tvWho.visibility = View.VISIBLE
-                binding.chipGroup.addView(Chip(context).apply {
-                    text = string
+        //아이템을 클릭했을 때 //TODO 리스트에서 삭제돼야함
+        adapter.setOnItemClickListener {
+            binding.chipGroup.addView(
+                (layoutInflater.inflate(
+                    R.layout.chip_layout,
+                    null,
+                    false
+                ) as Chip).apply {
+                    text = it //name 받아온 것
                     this.setChipBackgroundColorResource(R.color.chipbackground) //container color
                     this.setTextAppearance(R.style.ChipTextAppearance) //글자 색, 글자 크기 적용
-
-                    //this.setCloseIconResource(R.drawable.ic_friend_btn_remove) //TODO closebtn (이게 적용이 안되고 있음)
-
-                    binding.autoEtToWho.setText(null)
-                    Log.d("test,",binding.autoEtToWho.paddingLeft.toString())
-                    binding.autoEtToWho.setPadding(binding.autoEtToWho.paddingLeft + 250, 0, 0, 0)
-
-                    this.setCloseIconVisible(true)
+                    binding.etToWho.text.clear()
+                    binding.etToWho.setPadding(binding.etToWho.paddingLeft + 250, 0, 0, 0)
                     setOnCloseIconClickListener {
                         binding.chipGroup.removeView(this)
-                        binding.autoEtToWho.setPadding(
-                            binding.autoEtToWho.paddingLeft - 250,
-                            0,
-                            0,
-                            0
-                        )
+                        binding.etToWho.setPadding(binding.etToWho.paddingLeft - 250, 0, 0, 0)
                     }
                 })
-            }
         }
-    }
-
-    // 검색에 사용될 데이터를 리스트에 추가한다.
-/*
-    private fun settingList() {
-        friendlist!!.add(ApplyFriendData(1, "박유나"))
-        friendlist!!.add(ApplyFriendData(1, "박지현"))
-        friendlist!!.add(ApplyFriendData(1, "박승윤"))
-        friendlist!!.add(ApplyFriendData(1, "박나은"))
-        friendlist!!.add(ApplyFriendData(1, "박주혁"))
-        friendlist!!.add(ApplyFriendData(1, "박예림"))
-        friendlist!!.add(ApplyFriendData(1, "박한빈"))
-        friendlist!!.add(ApplyFriendData(1, "박수현"))
-    }*/
-
-
-    private fun settingList() {
-        friendlist!!.add("박유나")
-        friendlist!!.add("박지현")
-        friendlist!!.add("박승윤")
-        friendlist!!.add("박나은")
-        friendlist!!.add("박주혁")
-        friendlist!!.add("박예림")
-        friendlist!!.add("박한빈")
     }
 
     fun initFocusBackground() {
+        binding.etToWho.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                binding.tvWho.visibility = View.INVISIBLE
+                binding.rvFriend.visibility = View.VISIBLE
+            } else {
+                binding.rvFriend.visibility = View.INVISIBLE
+            }
+        }
+
         binding.etTitle.setOnFocusChangeListener { view, hasFocus ->
             if (hasFocus) {
                 binding.clContent.setBackgroundResource(R.drawable.rectangle_gray_10_stroke_pink)
@@ -166,19 +128,13 @@ class FirstApplyFragment : Fragment() {
     }
 
     fun initTextChangedListener() {
-        binding.autoEtToWho.addTextChangedListener {
-            if (binding.autoEtToWho.text.isNullOrBlank()) { //공백일 때
-                binding.tvWho.visibility = View.VISIBLE
-
-            } else { //뭐가 있을 때
-                binding.tvWho.visibility = View.INVISIBLE
-            }
+        binding.etToWho.addTextChangedListener {
             if (isNullorBlank()) { //셋 중 하나라도 작성 안 됐을 때
                 unactiveBtn()
             } else { //셋 다 작성했을 때
                 activeBtn()
             }
-            Log.d("test,",binding.chipGroup.childCount.toString())
+            Log.d("test,", binding.chipGroup.childCount.toString())
         }
 
         binding.etTitle.addTextChangedListener {
@@ -202,9 +158,9 @@ class FirstApplyFragment : Fragment() {
         }
     }
 
-    //여기서 하나라도 성립하면 true 반환
+    //여기서 하나라도 성립하면 true 반환 //TODO 칩 그룹 개수 버그 생김 수정 필요
     private fun isNullorBlank(): Boolean {
-        return binding.chipGroup.childCount==0 || binding.etTitle.text.isNullOrBlank() || binding.etDetail.text.isNullOrBlank() || (!binding.autoEtToWho.text.isNullOrBlank()) //TODO 여기 chip 개수 !!!!
+        return binding.chipGroup.childCount == 0 || binding.etTitle.text.isNullOrBlank() || binding.etDetail.text.isNullOrBlank() || (!binding.etToWho.text.isNullOrBlank()) //TODO 여기 chip 개수 !!!!
     }
 
     private fun activeBtn() {
@@ -218,6 +174,24 @@ class FirstApplyFragment : Fragment() {
         binding.btnNext.isClickable = false // 버튼 클릭할수 없게
         binding.btnNext.isEnabled = false // 버튼 비활성화
     }
+
+/*
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                binding.tvWho.visibility = View.INVISIBLE
+
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        callback.remove()
+    }
+*/
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/org/seemeet/seemeet/ui/apply/adapter/ApplyFriendAdapter.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/apply/adapter/ApplyFriendAdapter.kt
@@ -1,250 +1,48 @@
 package org.seemeet.seemeet.ui.apply.adapter
 
-import android.content.Context
-import org.seemeet.seemeet.data.local.ApplyFriendData
-import org.seemeet.seemeet.data.local.FriendData
-import java.util.ArrayList
-import androidx.annotation.NonNull
-
-import android.R
-import android.icu.util.UniversalTimeScale.toLong
-
-import android.view.LayoutInflater
-import android.view.View
-
-import android.view.ViewGroup
-import android.widget.*
-import androidx.annotation.LayoutRes
-import androidx.annotation.Nullable
-import org.seemeet.seemeet.databinding.FragmentFirstApplyBinding
-import org.seemeet.seemeet.databinding.ItemApplySearchFriendBinding
-
-
-/*
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import org.seemeet.seemeet.R
 import org.seemeet.seemeet.data.local.ApplyFriendData
 import org.seemeet.seemeet.databinding.ItemApplySearchFriendBinding
 
-class ApplyFriendAdapter : RecyclerView.Adapter<ApplyFriendAdapter.ApplyFriendViewHolder>(){
+class ApplyFriendAdapter : RecyclerView.Adapter<ApplyFriendAdapter.ApplyFriendViewHolder>() {
     val applyfriendList = mutableListOf<ApplyFriendData>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ApplyFriendViewHolder {
-        val binding = ItemApplySearchFriendBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    private var listener: ((String) -> Unit)? = null
+    fun setOnItemClickListener(listener: ((String) -> Unit)?) {
+        this.listener = listener
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ApplyFriendViewHolder {
+        val binding = ItemApplySearchFriendBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent, false
+        )
         return ApplyFriendViewHolder(binding)
     }
+
+    override fun getItemCount(): Int = applyfriendList.size
 
     override fun onBindViewHolder(holder: ApplyFriendViewHolder, position: Int) {
         holder.onBind(applyfriendList[position])
     }
 
-    override fun getItemCount(): Int = applyfriendList.size
-
-    class ApplyFriendViewHolder(private val binding: ItemApplySearchFriendBinding) : RecyclerView.ViewHolder(binding.root){
+    inner class ApplyFriendViewHolder(private val binding: ItemApplySearchFriendBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: ApplyFriendData) {
-            binding.ivApplyProfile.setImageResource(R.drawable.ic_x)
+            binding.ivApplyProfile.setImageResource(data.profile)
             binding.tvApplyName.text = data.name
-        }
-    }
-}
-*/
-/*
-class ApplyFriendAdapter(
-    context: Context,
-    resource: Int,
-    textViewResourceId: Int,
-    objects: List<ApplyFriendData>
 
-) : ArrayAdapter<ApplyFriendData>(context, resource, textViewResourceId, objects) {
-
-    private var suggests: MutableList<ApplyFriendData> = mutableListOf()
-
-    fun setSuggests(list: List<ApplyFriendData>) {
-        suggests.clear()
-        suggests.addAll(list)
-    }
-
-    override fun getCount(): Int {
-        return suggests.size
-    }
-
-    override fun getItem(position: Int): ApplyFriendData? {
-        return suggests[position]
-    }
-
-}*/
-/*
-class ApplyFriendAdapter(
-    private val mContext: Context,
-    private val mLayoutResourceId: Int,
-    friends: List<ApplyFriendData>
-) :
-    ArrayAdapter<ApplyFriendData>(mContext, mLayoutResourceId, friends) {
-    private val friend: MutableList<ApplyFriendData> = ArrayList(friends)
-    private var allCities: List<ApplyFriendData> = ArrayList(friends)
-
-    override fun getCount(): Int {
-        return friend.size
-    }
-    override fun getItem(position: Int): ApplyFriendData {
-        return friend[position]
-    }
-    override fun getItemId(position: Int): Long {
-        return friend[position].id.toLong()
-    }
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var convertView = convertView
-        if (convertView == null) {
-            val inflater = (mContext as Activity).layoutInflater
-            convertView = inflater.inflate(mLayoutResourceId, parent, false)
-        }
-        try {
-            val city: ApplyFriendData = getItem(position)
-            val cityAutoCompleteView = convertView!!.findViewById<View>(R.id.autoCompleteTextViewItem) as TextView
-            cityAutoCompleteView.text = city.definition
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return convertView!!
-    }
-}
-*/
-/*
-class AutoCompleteAdapter(context: Context, countryList: List<ApplyFriendData?>) :
-    ArrayAdapter<ApplyFriendData?>(context, 0, countryList) {
-    private val countryListFull: List<ApplyFriendData>
-
-    override fun getView(position: Int, @Nullable convertView: View, parent: ViewGroup): View {
-        var convertView: View = convertView
-        if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(
-                R.layout.country_autocomplete_row, parent, false
-            )
-        }
-        val textView: TextView = convertView.findViewById(R.id.text_view_name)
-        val imageView: ImageView = convertView.findViewById(R.id.image_view_flag)
-        val countryItem: ApplyFriendData? = getItem(position)
-        if (countryItem != null) {
-            textView.setText(countryItem.getCountryName())
-            imageView.setImageResource(countryItem.getFlagImage())
-        }
-        return convertView
-    }
-
-    init {
-        countryListFull = ArrayList<Any?>(countryList)
-    }
-}*/
-
-/*
-class ApplyFriendAdapter(context: Context, countryList: List<ApplyFriendData?>) :
-    ArrayAdapter<ApplyFriendData?>(context, 0, countryList) {
-    //데이터를 넣을 리스트
-    //private val countryListFull: List<ApplyFriendData>
-    private var _binding: ItemApplySearchFriendBinding? = null
-    val binding get() = _binding!!
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var convertView = convertView
-        if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(
-                org.seemeet.seemeet.R.layout.item_apply_search_friend, parent, false
-            )
-        }
-
-        //getItem(position) 코드로 자동완성 될 아이템을 가져온다
-        val countryItem: ApplyFriendData? = getItem(position)
-        if (countryItem != null) {
-            binding.tvApplyName.setText(countryItem.name)
-            binding.ivApplyProfile.setImageResource(countryItem.profile)
-        }
-        return convertView!!
-    }
-}
-*/
-
-/*
-    //-------------------------- 이 아래는 자동완성을 위한 검색어를 찾아주는 코드이다 --------------------------
-    override fun getFilter(): Filter {
-        return countryFilter
-    }
-
-    private val countryFilter: Filter = object : Filter() {
-        protected fun performFiltering(constraint: CharSequence?): FilterResults {
-            val results = FilterResults()
-            val suggestions: MutableList<ApplyFriendData> = ArrayList<ApplyFriendData>()
-            if (constraint == null || constraint.length == 0) {
-                suggestions.addAll(countryListFull)
-            } else {
-                val filterPattern = constraint.toString().toLowerCase().trim { it <= ' ' }
-                for (item in countryListFull) {
-                    if (item.getCountryName().toLowerCase().contains(filterPattern)) {
-                        suggestions.add(item)
-                    }
+            val pos = adapterPosition
+            if (pos != RecyclerView.NO_POSITION) {
+                itemView.setOnClickListener {
+                    listener?.invoke(data.name)
                 }
             }
-            results.values = suggestions
-            results.count = suggestions.size
-            return results
-        }
-
-        protected fun publishResults(constraint: CharSequence?, results: FilterResults) {
-            clear()
-            addAll(results.values as List<*>)
-            notifyDataSetChanged()
-        }
-
-        fun convertResultToString(resultValue: Any): CharSequence {
-            return (resultValue as ApplyFriendData).getCountryName()
         }
     }
-
-    init {
-        countryListFull = ArrayList<Any?>(countryList)
-    }
-}*/
-/*
-class PoiAdapter(context: Context, @LayoutRes private val layoutResource: Int, private val allPois: List<ApplyFriendData>):
-    ArrayAdapter<ApplyFriendData>(context, layoutResource, allPois),
-    Filterable {
-    private var mPois: List<ApplyFriendData> = allPois
-
-    override fun getCount(): Int {
-        return mPois.size
-    }
-
-    override fun getItem(p0: Int): ApplyFriendData? {
-        return mPois.get(p0)
-    }
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view: TextView = convertView as TextView? ?: LayoutInflater.from(context).inflate(layoutResource, parent, false) as TextView
-        view.text = "${mPois[position].name}"
-        return view
-    }
-
-    override fun getFilter(): Filter {
-        return object : Filter() {
-            override fun publishResults(charSequence: CharSequence?, filterResults: Filter.FilterResults) {
-                mPois = filterResults.values as List<ApplyFriendData>
-                notifyDataSetChanged()
-            }
-
-            override fun performFiltering(charSequence: CharSequence?): Filter.FilterResults {
-                val queryString = charSequence?.toString()?.toLowerCase()
-
-                val filterResults = Filter.FilterResults()
-                filterResults.values = if (queryString==null || queryString.isEmpty())
-                    allPois
-                else
-                    allPois.filter {
-                        it.name.toLowerCase().contains(queryString)
-                    }
-                return filterResults
-            }
-        }
-    }
-}*/
+}
