b6c7c53e33faa6d7e6296af575e5528b738c506a
최유림 <chzh1323@gmail.com>
2022-01-17 23:29:45 +0900

    [FEAT] Custom toast 생성

--- app/src/main/java/org/seemeet/seemeet/ui/registration/LoginActivity.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/registration/LoginActivity.kt
@@ -2,12 +2,13 @@ package org.seemeet.seemeet.ui.registration
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.text.method.PasswordTransformationMethod
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import org.seemeet.seemeet.R
-import org.seemeet.seemeet.ui.MainActivity
 import org.seemeet.seemeet.databinding.ActivityLoginBinding
+import org.seemeet.seemeet.ui.MainActivity
 
 class LoginActivity : AppCompatActivity() {
     private val binding: ActivityLoginBinding by lazy {
@@ -18,26 +19,44 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initClickListener()
-/*
+
+        var checkPWValue: Int = 0
+
         binding.etPw.setOnFocusChangeListener { view, hasFocus ->
             if (hasFocus) {
-                binding.clContent.setBackgroundResource(R.drawable.rectangle_gray_10_stroke_pink)
+                binding.ivPwShowHidden.visibility = View.VISIBLE
 
             } else {
-                binding.clContent.setBackgroundResource(R.drawable.rectangle_gray_10)
+                if (binding.etPw.text.isNullOrBlank()) {
+                    binding.ivPwShowHidden.visibility = View.INVISIBLE
+                } else binding.ivPwShowHidden.visibility = View.VISIBLE
             }
-        }*/
+        }
+        binding.ivPwShowHidden.setOnClickListener {
+            if (checkPWValue == 0) {
+                checkPWValue = 1
+                binding.ivPwShowHidden.setImageResource(R.drawable.ic_pw_show)
+                binding.etPw.transformationMethod = null
+            } else {
+                checkPWValue = 0
+                binding.ivPwShowHidden.setImageResource(R.drawable.ic_pw_hidden)
+                binding.etPw.transformationMethod = PasswordTransformationMethod.getInstance()
+            }
+        }
     }
 
     fun initClickListener() {
         binding.btnLogin.setOnClickListener {//버튼 눌렀을 때 아이디 틀림, 비번 틀림
-            if(!binding.etEmail.text.equals("hi@")){
-                Toast.makeText(this, "등록되지 않은 유저입니다.", Toast.LENGTH_SHORT).show()
+            /*
+            if (!binding.etPw.text.equals("0000")) {
+                CustomToast.createToast(this, "비밀번호가 틀렸습니다.")?.show()
             }
-            if(!binding.etPw.text.equals("0000")){
-                Toast.makeText(this, "비밀번호가 틀렸습니다.", Toast.LENGTH_SHORT).show()
+            if (!binding.etEmail.text.equals("hi@")) {
+                CustomToast.createToast(this, "등록되지 않은 유저입니다.")?.show()
+            }*/
+            if (!binding.etPw.text.isNullOrBlank() && !binding.etEmail.text.isNullOrBlank()) {
+                MainActivity.start(this)
             }
-
         }
         binding.tvRegister.setOnClickListener {
             val nextIntent = Intent(this, RegisterActivity::class.java)
@@ -48,18 +67,14 @@ class LoginActivity : AppCompatActivity() {
         binding.etEmail.addTextChangedListener {
             if (!isNullorBlank()) { //다 작성했을 때
                 activeBtn()
-            }else unactiveBtn()
+            } else unactiveBtn()
         }
         binding.etPw.addTextChangedListener {
             if (!isNullorBlank()) { //다 작성했을 때
                 activeBtn()
-            }else unactiveBtn()
+            } else unactiveBtn()
         }
-
-
     }
-
-
 
     private fun isNullorBlank(): Boolean {
         return binding.etPw.text.isNullOrBlank() || binding.etEmail.text.isNullOrBlank()
