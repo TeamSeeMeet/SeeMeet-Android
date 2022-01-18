efa76f3529f719db284c653f7b241794950118e2
최유림 <chzh1323@gmail.com>
2022-01-19 00:13:29 +0900

    [ADD] timepicker 다이얼로그 시도중

--- app/src/main/java/org/seemeet/seemeet/ui/apply/SecondApplyFragment.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/apply/SecondApplyFragment.kt
@@ -1,20 +1,75 @@
 package org.seemeet.seemeet.ui.apply
 
+import android.app.TimePickerDialog
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.seemeet.seemeet.R
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import org.seemeet.seemeet.databinding.FragmentSecondApplyBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SecondApplyFragment : Fragment() {
 
+    private var _binding: FragmentSecondApplyBinding? = null
+    val binding get() = _binding!!
+    var timeString = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_second_apply, container, false)
+        // Inflate the layout for this fragment
+        _binding = FragmentSecondApplyBinding.inflate(layoutInflater)
+
+/*
+        binding.time.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                timeString = "${hourOfDay}시 ${minute}분"
+                binding.time.text = "시간 : " + timeString
+            }
+            TimePickerDialog(
+                context,
+                timeSetListener,
+                cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE),
+                true
+            ).show()
+        }*/
+
+        binding.time.text =
+            Calendar.getInstance().get(Calendar.HOUR).toString() + Calendar.getInstance()
+                .get(Calendar.MINUTE).toString()
+
+
+        binding.time.setOnClickListener {
+            getTime(binding.time, requireContext(), binding.time.text.toString())
+        }
+
+
+
+        return binding.root
+    }
+
+    fun getTime(textView: TextView, context: Context, string: String){
+
+        val cal = Calendar.getInstance()
+
+        String
+
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+
+            textView.text = SimpleDateFormat("aa hh:mm").format(cal.time)
+        }
+
+        TimePickerDialog(context, timeSetListener, cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE), false).show()
+
     }
 
 }
