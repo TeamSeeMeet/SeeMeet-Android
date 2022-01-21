24ad16932670b9e938cde23e3a9506bdba435d7c
rkdmf1026 <rkdmf1026@naver.com>
2022-01-21 21:34:59 +0900

    [ADD] postApplyInvitation api

--- app/src/main/java/org/seemeet/seemeet/data/local/ApplyFriendData.kt
+++ app/src/main/java/org/seemeet/seemeet/data/local/ApplyFriendData.kt
@@ -4,6 +4,6 @@ import org.seemeet.seemeet.data.model.response.invitation.UserData
 import java.io.Serializable
 
 data class ApplyFriendData( //첫번째 약속신청 뷰에서 두번쨰로 넘길 때 주려고 로컬로 데이터 클래스 만듦
-    val userId : Int,
-    val userName : String
+    val id : Int,
+    val username : String
 ) : Serializable
