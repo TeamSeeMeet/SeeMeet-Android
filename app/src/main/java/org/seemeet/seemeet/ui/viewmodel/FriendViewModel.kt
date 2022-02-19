51b54b30ac5455bc213c0ce1a02842e79d2d2cb5
Hyuna Kim <akxmcse@gmail.com>
2022-02-20 02:02:28 +0900

    [FEAT] FriendView 네트워크 예외 처리

--- app/src/main/java/org/seemeet/seemeet/ui/viewmodel/FriendViewModel.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/viewmodel/FriendViewModel.kt
@@ -1,33 +1,26 @@
 package org.seemeet.seemeet.ui.viewmodel
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.seemeet.seemeet.data.SeeMeetSharedPreference
 import org.seemeet.seemeet.data.api.RetrofitBuilder
 import org.seemeet.seemeet.data.model.response.friend.ResponseFriendList
-import retrofit2.HttpException
 
-class FriendViewModel(application: Application) : AndroidViewModel(application) {
+class FriendViewModel(application: Application) : BaseViewModel(application) {
 
     private val _friendList = MutableLiveData<ResponseFriendList>()
     val friendList: LiveData<ResponseFriendList>
         get() = _friendList
 
     //  친구 목록 가져오기
-    fun requestFriendList() = viewModelScope.launch(Dispatchers.IO) {
-        try {
-            _friendList.postValue(
-                RetrofitBuilder.friendService.getFriendList(
-                    SeeMeetSharedPreference.getToken()
-                )
+    fun requestFriendList() = viewModelScope.launch(exceptionHandler) {
+        _friendList.postValue(
+            RetrofitBuilder.friendService.getFriendList(
+                SeeMeetSharedPreference.getToken()
             )
-        } catch (e: HttpException) {
-            e.printStackTrace()
-        }
+        )
     }
 }
