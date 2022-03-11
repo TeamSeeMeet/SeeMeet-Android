1e113bcb3ddb1c8fb82f24b7ad86abe7189f4d7b
최유림 <chzh1323@gmail.com>
2022-03-11 13:07:11 +0900

    [FEAT] 메인로그인 뷰/ 이메일로 로그인 뷰

--- app/src/main/java/org/seemeet/seemeet/ui/main/home/HomeFragment.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/main/home/HomeFragment.kt
@@ -21,7 +21,7 @@ import org.seemeet.seemeet.ui.friend.FriendActivity
 import org.seemeet.seemeet.ui.main.MainActivity
 import org.seemeet.seemeet.ui.main.home.adapter.ReminderListAdapter
 import org.seemeet.seemeet.ui.notification.NotificationActivity
-import org.seemeet.seemeet.ui.registration.LoginActivity
+import org.seemeet.seemeet.ui.registration.LoginMainActivity
 import org.seemeet.seemeet.ui.viewmodel.BaseViewModel
 import org.seemeet.seemeet.ui.viewmodel.HomeViewModel
 import org.seemeet.seemeet.util.calDday
@@ -90,7 +90,7 @@ class HomeFragment : Fragment() {
 
             nvMypage.clMypageLogin.setOnClickListener{
                 if(!getLogin())
-                    LoginActivity.start(requireContext())
+                    LoginMainActivity.start(requireContext())
             }
 
             nvMypage.clMypageContent.setOnClickListener {
@@ -100,7 +100,7 @@ class HomeFragment : Fragment() {
 
             nvMypage.tvEmail.setOnClickListener {
                 SeeMeetSharedPreference.clearStorage()
-                LoginActivity.start(requireContext())
+                LoginMainActivity.start(requireContext())
             }
         }
 
