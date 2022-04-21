11277393e14f210f01e631ed6392f49ae7676231
최유림 <chzh1323@gmail.com>
2022-04-22 00:55:33 +0900

    [REFACTOR] registerView 리팩토링

--- app/src/main/java/org/seemeet/seemeet/ui/registration/RegisterActivity.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/registration/RegisterActivity.kt
@@ -12,16 +12,12 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.Observer
 import org.seemeet.seemeet.R
 import org.seemeet.seemeet.databinding.ActivityRegisterBinding
 import org.seemeet.seemeet.ui.main.MainActivity
 import org.seemeet.seemeet.ui.viewmodel.BaseViewModel
 import org.seemeet.seemeet.ui.viewmodel.RegisterViewModel
-import org.seemeet.seemeet.util.activeBtn
-import org.seemeet.seemeet.util.inactiveBtn
 import org.seemeet.seemeet.util.makeInVisible
 import org.seemeet.seemeet.util.makeVisible
 import retrofit2.HttpException
@@ -39,6 +35,8 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.registerViewModel = viewModel
+        binding.lifecycleOwner = this
         initClickListener()
         statusObserver()
     }
@@ -50,7 +48,6 @@ class RegisterActivity : AppCompatActivity() {
             } else {
                 binding.etEmailRegister.requestFocus()
                 binding.tvWarningEmail.text = resources.getString(R.string.register_registeredEmail)
-                binding.tvWarningEmail.makeVisible()
             }
         })
 
@@ -102,44 +99,6 @@ class RegisterActivity : AppCompatActivity() {
             finish()
         }
 
-        binding.etEmailRegister.addTextChangedListener {
-            if (pattern.matcher(binding.etEmailRegister.text).matches()) {
-                //이메일 맞음
-                binding.tvWarningEmail.makeInVisible()
-            } else {
-                //이메일 아님
-                binding.tvWarningEmail.text = resources.getString(R.string.register_incorrectEmail)
-                binding.tvWarningEmail.makeVisible()
-            }
-            if (isNullOrBlank()) {
-                binding.btnRegisterNext.inactiveBtn(R.drawable.rectangle_gray02_10)
-            } else {
-                binding.btnRegisterNext.activeBtn()
-            }
-            if (binding.etEmailRegister.text.isNullOrBlank())
-                binding.tvWarningEmail.makeInVisible()
-        }
-
-        binding.etPw.addTextChangedListener {
-            if (binding.etPw.text.length < 8) {
-                binding.tvWarningPw.text = resources.getString(R.string.register_lengthPassword)
-                binding.tvWarningPw.makeVisible()
-            } else { //8자 이상인 경우
-                if (!isPasswordFormat(binding.etPw.text.toString())) {//영문, 숫자 , 특수문자 중 2가지 이상 사용안했을 경우
-                    binding.tvWarningPw.text = resources.getString(R.string.register_formatPassword)
-                    binding.tvWarningPw.makeVisible()
-                } else binding.tvWarningPw.makeInVisible()
-            }
-            if (binding.etPw.text.isNullOrBlank())
-                binding.tvWarningPw.makeInVisible()
-
-            if (isNullOrBlank()) {
-                binding.btnRegisterNext.inactiveBtn(R.drawable.rectangle_gray02_10)
-            } else {
-                binding.btnRegisterNext.activeBtn()
-            }
-        }
-
         binding.ivPwShowHidden.setOnClickListener {
             if (pwValue == LoginActivity.HIDDEN_PW) {
                 pwValue = LoginActivity.SHOW_PW
@@ -174,23 +133,6 @@ class RegisterActivity : AppCompatActivity() {
             }
         }
 
-        binding.etCheckpw.addTextChangedListener {
-            if (binding.etCheckpw.text.toString() != binding.etPw.text.toString()) {
-                binding.tvWarningCheckpw.text =
-                    resources.getString(R.string.register_incorrectPassword)
-                binding.tvWarningCheckpw.makeVisible()
-            } else binding.tvWarningCheckpw.makeInVisible() //일치할 경우 tv 안 뜨게
-
-            if (binding.etCheckpw.text.isNullOrBlank())
-                binding.tvWarningCheckpw.makeInVisible()
-
-            if (isNullOrBlank()) {
-                binding.btnRegisterNext.inactiveBtn(R.drawable.rectangle_gray02_10)
-            } else {
-                binding.btnRegisterNext.activeBtn()
-            }
-        }
-
         binding.etPw.setOnFocusChangeListener { _, hasFocus ->
             if (hasFocus) {
                 binding.ivPwShowHidden.makeVisible()
@@ -201,6 +143,7 @@ class RegisterActivity : AppCompatActivity() {
                 } else binding.ivPwShowHidden.makeVisible()
             }
         }
+
         binding.etCheckpw.setOnFocusChangeListener { _, hasFocus ->
             if (hasFocus) {
                 binding.ivCheckpwShowHidden.makeVisible()
@@ -211,19 +154,6 @@ class RegisterActivity : AppCompatActivity() {
                 } else binding.ivCheckpwShowHidden.makeVisible()
             }
         }
-    }
-
-    private fun isPasswordFormat(password: String): Boolean {
-        return password.matches(PASSWORD_FORMAT.toRegex())
-    }
-
-    private fun isNullOrBlank(): Boolean { //하나라도 성립하면 true 반환 (= 4개 중에 하나라도 이상한게 있을 때)
-        return binding.tvWarningEmail.isVisible ||
-                binding.tvWarningCheckpw.isVisible ||
-                binding.tvWarningPw.isVisible ||
-                binding.etEmailRegister.text.isNullOrBlank() ||
-                binding.etPw.text.isNullOrBlank() ||
-                binding.etCheckpw.text.isNullOrBlank()
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
--- app/src/main/java/org/seemeet/seemeet/util/BindingAdapters.kt
+++ app/src/main/java/org/seemeet/seemeet/util/BindingAdapters.kt
@@ -7,15 +7,18 @@ import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
 import android.text.style.RelativeSizeSpan
 import android.text.style.StyleSpan
+import android.util.Patterns
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 import androidx.databinding.BindingAdapter
-
 import androidx.recyclerview.widget.RecyclerView
 import org.seemeet.seemeet.R
 import org.seemeet.seemeet.data.model.response.invitation.GuestX
 import org.seemeet.seemeet.data.model.response.invitation.SendGuest
 import org.seemeet.seemeet.data.model.response.invitation.SendRespondent
+import org.seemeet.seemeet.ui.registration.RegisterActivity
+import java.util.regex.Pattern
 
 object BindingAdapters {
     //더미용
@@ -235,12 +238,17 @@ object BindingAdapters {
 
     @JvmStatic
     @BindingAdapter("setWordPinkBoldAllText", "setPinkBoldText")
-    fun setWordPinkBoldText(textView: TextView, text : String, pink : String) {
+    fun setWordPinkBoldText(textView: TextView, text: String, pink: String) {
         val start = text.indexOf(pink)
         val end = start + pink.length
 
         val ss = SpannableStringBuilder(text)
-        ss.setSpan(ForegroundColorSpan(Color.parseColor("#FA555C")), start, end, Spannable.SPAN_EXCLUSIVE_INCLUSIVE)
+        ss.setSpan(
+            ForegroundColorSpan(Color.parseColor("#FA555C")),
+            start,
+            end,
+            Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+        )
         ss.setSpan(StyleSpan(Typeface.BOLD), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         ss.setSpan(RelativeSizeSpan(1.2f), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         textView.text = ss
@@ -285,5 +293,62 @@ object BindingAdapters {
         if (recyclerView.adapter is BindingRecyclerViewAdapter<*> && data != null) {
             (recyclerView.adapter as BindingRecyclerViewAdapter<T>).setData(data)
         }
+    }
+
+    //여기서는 버튼 활성화 비활성화
+    @JvmStatic
+    @BindingAdapter("warningEmail", "warningPw", "warningCheckpw", "etEmail", "etPw", "etCheckpw")
+    fun registerNextBtn(
+        button: AppCompatButton,
+        warningEmail: String?,
+        warningPw: String?,
+        warningCheckpw: String?,
+        etEmail: String?,
+        etPw: String?,
+        etCheckpw: String?
+    ) {
+        val state: Boolean =
+            (!warningEmail.isNullOrBlank() || !warningPw.isNullOrBlank() || !warningCheckpw.isNullOrBlank() || etEmail.isNullOrBlank() || etPw.isNullOrBlank() || etCheckpw.isNullOrBlank())
+        if (state) {
+            button.inactiveBtn(R.drawable.rectangle_gray02_10)
+        } else
+            button.activeBtn()
+    }
+
+    @JvmStatic
+    @BindingAdapter("warningEmailtext")
+    fun setWarningEmailtext(textView: TextView, etEmail: String?) {
+        val pattern: Pattern = Patterns.EMAIL_ADDRESS
+        if (pattern.matcher(etEmail).matches()) {
+            //이메일 형식 맞음
+            textView.text = ""
+        } else
+            textView.setText(R.string.register_incorrectEmail)
+        if(etEmail.isNullOrBlank()) textView.text=""
+    }
+
+    @JvmStatic
+    @BindingAdapter("warningPwtext")
+    fun setWarningPwtext(textView: TextView, etPw: String?) {
+        if (etPw?.length ?: 0 < 8) {
+            textView.setText(R.string.register_lengthPassword)
+        } else { // 8자 이상인 경우
+            if (!(etPw?.matches(RegisterActivity.PASSWORD_FORMAT.toRegex())!!)) { // 영문, 숫자 , 특수문자 중 2가지 이상 사용안했을 경우
+                textView.setText(R.string.register_formatPassword)
+            } else textView.text = ""
+        }
+        if (etPw.isNullOrBlank())
+            textView.text = ""
+    }
+
+    @JvmStatic
+    @BindingAdapter("warningCheckpwtext", "etPw")
+    fun setWarningCheckpwtext(textView: TextView, etCheckpw: String?, etPw: String?) {
+        if (etCheckpw != etPw) {
+            textView.setText(R.string.register_incorrectPassword)
+        } else textView.text = "" // 일치할 경우 tv 안 뜨게
+
+        if (etCheckpw.isNullOrBlank())
+            textView.text = ""
     }
 }
