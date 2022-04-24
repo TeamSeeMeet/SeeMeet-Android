1d7b5fe5e2348df88502d63ba558ffb3dc7e4c86
Lee <rkdmf1026@naver.com>
2022-04-24 21:23:06 +0900

    [ADD] Connect kakaoLogin api

--- app/src/main/java/org/seemeet/seemeet/ui/registration/LoginMainActivity.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/registration/LoginMainActivity.kt
@@ -3,7 +3,18 @@ package org.seemeet.seemeet.ui.registration
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import org.seemeet.seemeet.data.api.LoginService
+import org.seemeet.seemeet.data.api.RetrofitBuilder
+import org.seemeet.seemeet.data.model.request.login.RequestKakaoLogin
 import org.seemeet.seemeet.databinding.ActivityLoginMainBinding
 
 
@@ -11,6 +22,9 @@ class LoginMainActivity : AppCompatActivity() {
     private val binding: ActivityLoginMainBinding by lazy {
         ActivityLoginMainBinding.inflate(layoutInflater)
     }
+
+    //kakao
+    private val userApiClient = UserApiClient.instance
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +34,7 @@ class LoginMainActivity : AppCompatActivity() {
 
     fun initClickListener() {
         binding.kakaoLogo.setOnClickListener {
-
+            getKaKaoToken()
         }
 
         binding.tvEmailRegister.setOnClickListener {
@@ -33,6 +47,59 @@ class LoginMainActivity : AppCompatActivity() {
             val nextIntent = Intent(this, LoginActivity::class.java)
             startActivity(nextIntent)
             //finish()
+        }
+    }
+
+    private fun getKaKaoToken() {
+        if (userApiClient.isKakaoTalkLoginAvailable(this)) {
+            Log.d("kakaoLogin", "카카오톡으로 로그인 가능")
+            userApiClient.loginWithKakaoTalk(
+                this,
+                callback = kakaoLoginCallback
+            )
+        } else {
+            Log.d("kakaoLogin", "카카오톡으로 로그인 불가능")
+            userApiClient.loginWithKakaoAccount(
+                this,
+                callback = kakaoLoginCallback
+            )
+        }
+    }
+
+    private val kakaoLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (token != null) {
+            Log.d("kakaoLogin", "카카오계정 로그인 성공 ${token.accessToken}")
+            trySeeMeetLogin(token.accessToken)
+            // 사용자 정보 가져오기
+            userApiClient.me { user, error ->
+                if (error != null) {
+                    Log.d("kakaoLogin", "카카오계정 사용자 정보 가져오기 실패")
+                } else if (user != null) {
+                    user.id
+                    Log.d(
+                        "kakaoLogin",
+                        "카카오계정 사용자 정보 가져오기 성공\n" +
+                                "닉네임 = ${user.kakaoAccount?.profile?.nickname}\n " +
+                                "프사 : ${user.kakaoAccount?.profile?.profileImageUrl}\n" +
+                                "이메일 : ${user.kakaoAccount?.email}\n" +
+                                "아이디 : ${user.id}\n" +
+                                "이름 : ${user.kakaoAccount?.name}"
+                    )
+                }
+            }
+        } else if (error != null) {
+            Log.d("kakaoLogin", error.toString())
+            Toast.makeText(this, "다시 로그인해주세요", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun trySeeMeetLogin(socialToken: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                RetrofitBuilder.loginService.postKakaoLogin(RequestKakaoLogin(socialToken, "kakao"))
+            } catch (e: Exception) {
+                Log.e("network error", e.toString())
+            }
         }
     }
 
