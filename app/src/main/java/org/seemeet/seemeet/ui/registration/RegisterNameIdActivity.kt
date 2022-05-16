d2c372d41ab9f856980d0a1d650bf0854afdf399
Lee <rkdmf1026@naver.com>
2022-05-16 14:57:46 +0900

    resolve conflict

--- app/src/main/java/org/seemeet/seemeet/ui/registration/RegisterNameIdActivity.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/registration/RegisterNameIdActivity.kt
@@ -1,39 +1,26 @@
 package org.seemeet.seemeet.ui.registration
 
-<<<<<<< HEAD
+
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import androidx.core.widget.addTextChangedListener
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.seemeet.seemeet.R
-import org.seemeet.seemeet.data.SeeMeetSharedPreference
-import org.seemeet.seemeet.data.api.RetrofitBuilder
-import org.seemeet.seemeet.data.model.request.login.RequestKakaoLogin
-import org.seemeet.seemeet.data.model.request.register.RequestRegisterNameId
-import org.seemeet.seemeet.data.model.response.login.ExUser
-import org.seemeet.seemeet.databinding.ActivityRegisterNameIdActivityBinding
-import org.seemeet.seemeet.ui.main.MainActivity
-import org.seemeet.seemeet.util.activeBtn
-import org.seemeet.seemeet.util.inactiveBtn
-import org.seemeet.seemeet.util.makeInVisible
-import org.seemeet.seemeet.util.makeVisible
-=======
 import android.graphics.Rect
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.seemeet.seemeet.data.SeeMeetSharedPreference
+import org.seemeet.seemeet.data.api.RetrofitBuilder
+import org.seemeet.seemeet.data.model.request.register.RequestRegisterNameId
+import org.seemeet.seemeet.data.model.response.login.ExUser
 import org.seemeet.seemeet.databinding.ActivityRegisterNameIdActivityBinding
+import org.seemeet.seemeet.ui.main.MainActivity
 import org.seemeet.seemeet.ui.viewmodel.RegisterNameIdViewModel
->>>>>>> develop
 import java.util.regex.Pattern
 
 class RegisterNameIdActivity : AppCompatActivity() {
@@ -90,7 +77,6 @@ class RegisterNameIdActivity : AppCompatActivity() {
         }
     }
 
-<<<<<<< HEAD
     // sharedPreference setting
     private fun setSharedPreference(list : ExUser) {
         SeeMeetSharedPreference.setUserId(list.id)
@@ -98,9 +84,6 @@ class RegisterNameIdActivity : AppCompatActivity() {
         SeeMeetSharedPreference.setUserName(list.username)
     }
 
-    private fun isNumberFormat(password: String): Boolean {
-        return Pattern.matches("^[0-9]*\$", password)
-=======
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
         val focusView = currentFocus
         if (focusView != null && ev != null) {
@@ -116,7 +99,6 @@ class RegisterNameIdActivity : AppCompatActivity() {
             }
         }
         return super.dispatchTouchEvent(ev)
->>>>>>> develop
     }
 
     companion object {
@@ -135,9 +117,7 @@ class RegisterNameIdActivity : AppCompatActivity() {
         fun isdotFormat(password: String): Boolean {
             return Pattern.matches("^[.]*\$", password)
         }
-    }
 
-    companion object {
         fun start(context: Context) {
             val intent = Intent(context, RegisterNameIdActivity::class.java)
             context.startActivity(intent)
