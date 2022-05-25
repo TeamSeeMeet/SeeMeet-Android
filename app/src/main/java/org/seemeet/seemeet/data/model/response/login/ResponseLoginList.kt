daf97d30fb693b76b4811e7887ebba13a0c7056c
yulim <yulim13sky@naver.com>
2022-05-25 16:58:11 +0900

    [CHORE] SeeMeetSharedPreference 닉네임으로 변경

--- app/src/main/java/org/seemeet/seemeet/data/model/response/login/ResponseLoginList.kt
+++ app/src/main/java/org/seemeet/seemeet/data/model/response/login/ResponseLoginList.kt
@@ -1,7 +1,5 @@
 package org.seemeet.seemeet.data.model.response.login
 
-import java.util.*
-
 data class ResponseLoginList(
     val status: Int,
     val success: Boolean,
@@ -9,20 +7,7 @@ data class ResponseLoginList(
     val data: Data
 )
 
-data class UserInfo(
-    val id: Int,
-    val email: String,
-    val idFirebase: String,
-    val username: String,
-    val gender: String,
-    val birth: Date,
-    val isNoticed: Boolean,
-    val createdAt: Date,
-    val updatedAt: Date,
-    val isDeleted: Boolean
-)
-
 data class Data(
-    val user: UserInfo,
+    val user: ExUser,
     val accesstoken: String
 )
