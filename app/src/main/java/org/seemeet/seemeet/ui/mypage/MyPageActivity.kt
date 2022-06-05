21e1275b0016420d5e3fa7c9cb30255646b5b0a5
yulim <yulim13sky@naver.com>
2022-06-06 02:46:39 +0900

    [REFACTOR] 마이페이지 fragment 삭제, activity 한 개로 구현

--- app/src/main/java/org/seemeet/seemeet/ui/mypage/MyPageActivity.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/mypage/MyPageActivity.kt
@@ -11,6 +11,8 @@ import org.seemeet.seemeet.ui.main.home.ChangePwActivity
 import org.seemeet.seemeet.ui.registration.LoginMainActivity
 
 class MyPageActivity : AppCompatActivity() {
+    private var profile_position = DEFAULT
+    private var nameId_position = DEFAULT
     private val binding: ActivityMyPageBinding by lazy {
         ActivityMyPageBinding.inflate(layoutInflater)
     }
@@ -22,14 +24,78 @@ class MyPageActivity : AppCompatActivity() {
         initClickListener()
     }
 
-    fun initSetting(){
-        if(!SeeMeetSharedPreference.getSocialLogin())
+    fun initSetting() {
+        if (!SeeMeetSharedPreference.getSocialLogin())
             binding.clChangepw.visibility = View.VISIBLE
+        binding.etMypageName.setText(SeeMeetSharedPreference.getUserName())
+        binding.etMypageId.setText(SeeMeetSharedPreference.getUserId())
     }
 
     fun initClickListener() {
         binding.backMypage.setOnClickListener {
             finish()
+        }
+
+        binding.btnProfileEditOrSave.setOnClickListener {
+            when (profile_position) {
+                DEFAULT -> {
+                    binding.btnProfileEditOrSave.text = "프로필 사진 저장"
+                    binding.btnSelectImage.visibility = View.VISIBLE
+                    profile_position = ONEDITPROFILE
+                }
+                ONEDITPROFILE -> {
+                    //프로필 사진 바꾸는 서버 연결
+                    binding.btnProfileEditOrSave.text = "프로필 사진 편집"
+                    binding.btnSelectImage.visibility = View.INVISIBLE
+                    profile_position = DEFAULT
+                }
+            }
+        }
+
+        binding.btnSelectImage.setOnClickListener {
+            //갤러리로 들어가기
+        }
+
+        binding.btnEditOrSave.setOnClickListener {
+            when (nameId_position) {
+                DEFAULT -> {
+                    //수정하기 버튼을 누를 때
+                    binding.btnMypageCancel.visibility = View.VISIBLE
+                    binding.btnEditOrSave.text = "저장"
+                    binding.etMypageName.setText(SeeMeetSharedPreference.getUserName())
+                    binding.etMypageName.isEnabled = true
+                    binding.mypageLine.visibility = View.VISIBLE
+                    binding.etMypageId.setText(SeeMeetSharedPreference.getUserId())
+                    binding.etMypageId.isEnabled = true
+                    binding.mypageLine2.visibility = View.VISIBLE
+                    nameId_position = ONEDITNAMEID
+                }
+                ONEDITNAMEID -> {
+                    //저장하기 버튼을 누를 때
+                    //이름, 아이디 바꾸는 서버 연결
+                    binding.btnMypageCancel.visibility = View.INVISIBLE
+                    binding.btnEditOrSave.text = "수정"
+                    binding.etMypageName.setText(SeeMeetSharedPreference.getUserName())
+                    binding.etMypageName.isEnabled = false
+                    binding.mypageLine.visibility = View.INVISIBLE
+                    binding.etMypageId.setText(SeeMeetSharedPreference.getUserId())
+                    binding.etMypageId.isEnabled = false
+                    binding.mypageLine2.visibility = View.INVISIBLE
+                    nameId_position = DEFAULT
+                }
+            }
+        }
+
+        binding.btnMypageCancel.setOnClickListener {
+            binding.btnMypageCancel.visibility = View.INVISIBLE
+            binding.btnEditOrSave.text = "수정"
+            binding.etMypageName.setText(SeeMeetSharedPreference.getUserName())
+            binding.etMypageName.isEnabled = false
+            binding.mypageLine.visibility = View.INVISIBLE
+            binding.etMypageId.setText(SeeMeetSharedPreference.getUserId())
+            binding.etMypageId.isEnabled = false
+            binding.mypageLine2.visibility = View.INVISIBLE
+            nameId_position = DEFAULT
         }
 
         binding.clChangepw.setOnClickListener {
@@ -81,5 +147,9 @@ class MyPageActivity : AppCompatActivity() {
             val intent = Intent(context, MyPageActivity::class.java)
             context.startActivity(intent)
         }
+
+        const val DEFAULT = 1
+        const val ONEDITPROFILE = 2
+        const val ONEDITNAMEID = 2
     }
 }
