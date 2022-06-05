87370b719bd82fd8215343f35f42df09bdfcf339
yulim <yulim13sky@naver.com>
2022-06-06 04:32:06 +0900

    [FEAT] 마이페이지에서 현재 프로필 사진 띄우기

--- app/src/main/java/org/seemeet/seemeet/ui/mypage/MyPageActivity.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/mypage/MyPageActivity.kt
@@ -5,9 +5,9 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import com.bumptech.glide.Glide
 import org.seemeet.seemeet.data.SeeMeetSharedPreference
 import org.seemeet.seemeet.databinding.ActivityMyPageBinding
@@ -17,6 +17,8 @@ import org.seemeet.seemeet.ui.registration.LoginMainActivity
 class MyPageActivity : AppCompatActivity() {
     private var profile_position = DEFAULT
     private var nameId_position = DEFAULT
+    var currentImageUrl: String? = SeeMeetSharedPreference.getUserProfile()
+
     private val binding: ActivityMyPageBinding by lazy {
         ActivityMyPageBinding.inflate(layoutInflater)
     }
@@ -31,33 +33,14 @@ class MyPageActivity : AppCompatActivity() {
     fun initSetting() {
         if (!SeeMeetSharedPreference.getSocialLogin())
             binding.clChangepw.visibility = View.VISIBLE
+
+        if (!currentImageUrl.isNullOrEmpty()) {
+            Glide.with(this).load(currentImageUrl!!.toUri()).circleCrop()
+                .into(binding.ivMypageProfile)
+        }
+
         binding.etMypageName.setText(SeeMeetSharedPreference.getUserName())
         binding.etMypageId.setText(SeeMeetSharedPreference.getUserId())
-    }
-
-    private val OPEN_GALLERY = 1
-    private fun openGallery() {
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.setType("image/*")
-        startActivityForResult(intent, OPEN_GALLERY)
-    }
-
-    @Override
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == OPEN_GALLERY) {
-                var currentImageUrl: Uri? = data?.data
-
-                try {
-                    Glide.with(this).load(currentImageUrl).circleCrop().into(binding.ivMypageProfile)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            }
-        } else {
-        }
     }
 
     fun initClickListener() {
@@ -74,6 +57,7 @@ class MyPageActivity : AppCompatActivity() {
                 }
                 ONEDITPROFILE -> {
                     //프로필 사진 바꾸는 서버 연결
+                    SeeMeetSharedPreference.setUserProfile(currentImageUrl)
                     binding.btnProfileEditOrSave.text = "프로필 사진 편집"
                     binding.btnSelectImage.visibility = View.INVISIBLE
                     profile_position = DEFAULT
@@ -90,42 +74,18 @@ class MyPageActivity : AppCompatActivity() {
             when (nameId_position) {
                 DEFAULT -> {
                     //수정하기 버튼을 누를 때
-                    binding.btnMypageCancel.visibility = View.VISIBLE
-                    binding.btnEditOrSave.text = "저장"
-                    binding.etMypageName.setText(SeeMeetSharedPreference.getUserName())
-                    binding.etMypageName.isEnabled = true
-                    binding.mypageLine.visibility = View.VISIBLE
-                    binding.etMypageId.setText(SeeMeetSharedPreference.getUserId())
-                    binding.etMypageId.isEnabled = true
-                    binding.mypageLine2.visibility = View.VISIBLE
-                    nameId_position = ONEDITNAMEID
+                    BtnEdit()
                 }
                 ONEDITNAMEID -> {
                     //저장하기 버튼을 누를 때
                     //이름, 아이디 바꾸는 서버 연결
-                    binding.btnMypageCancel.visibility = View.INVISIBLE
-                    binding.btnEditOrSave.text = "수정"
-                    binding.etMypageName.setText(SeeMeetSharedPreference.getUserName())
-                    binding.etMypageName.isEnabled = false
-                    binding.mypageLine.visibility = View.INVISIBLE
-                    binding.etMypageId.setText(SeeMeetSharedPreference.getUserId())
-                    binding.etMypageId.isEnabled = false
-                    binding.mypageLine2.visibility = View.INVISIBLE
-                    nameId_position = DEFAULT
+                    BtnSave()
                 }
             }
         }
 
         binding.btnMypageCancel.setOnClickListener {
-            binding.btnMypageCancel.visibility = View.INVISIBLE
-            binding.btnEditOrSave.text = "수정"
-            binding.etMypageName.setText(SeeMeetSharedPreference.getUserName())
-            binding.etMypageName.isEnabled = false
-            binding.mypageLine.visibility = View.INVISIBLE
-            binding.etMypageId.setText(SeeMeetSharedPreference.getUserId())
-            binding.etMypageId.isEnabled = false
-            binding.mypageLine2.visibility = View.INVISIBLE
-            nameId_position = DEFAULT
+            BtnSave()
         }
 
         binding.clChangepw.setOnClickListener {
@@ -170,6 +130,54 @@ class MyPageActivity : AppCompatActivity() {
             })
             dialogView.show(supportFragmentManager, "send wish checkbox time")
         }
+    }
+
+    private val OPEN_GALLERY = 1
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.setType("image/*")
+        startActivityForResult(intent, OPEN_GALLERY)
+    }
+
+    @Override
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == OPEN_GALLERY) {
+                var ImageUrl: Uri? = data?.data
+                currentImageUrl = ImageUrl.toString()
+                try {
+                    Glide.with(this).load(ImageUrl).circleCrop()
+                        .into(binding.ivMypageProfile)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        } else {
+        }
+    }
+
+    private fun BtnEdit() {
+        binding.btnMypageCancel.visibility = View.VISIBLE
+        binding.btnEditOrSave.text = "저장"
+        binding.etMypageName.isEnabled = true
+        binding.mypageLine.visibility = View.VISIBLE
+        binding.etMypageId.isEnabled = true
+        binding.mypageLine2.visibility = View.VISIBLE
+        nameId_position = ONEDITNAMEID
+    }
+
+    private fun BtnSave() {
+        binding.btnMypageCancel.visibility = View.INVISIBLE
+        binding.btnEditOrSave.text = "수정"
+        binding.etMypageName.setText(SeeMeetSharedPreference.getUserName())
+        binding.etMypageName.isEnabled = false
+        binding.mypageLine.visibility = View.INVISIBLE
+        binding.etMypageId.setText(SeeMeetSharedPreference.getUserId())
+        binding.etMypageId.isEnabled = false
+        binding.mypageLine2.visibility = View.INVISIBLE
+        nameId_position = DEFAULT
     }
 
     companion object {
