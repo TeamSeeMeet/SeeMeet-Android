2ea804c16262f675861d1a9c413d0dba38c0dbab
yulim <yulim13sky@naver.com>
2022-06-06 03:42:14 +0900

    [FEAT] 갤러리 들어가서 사진 원형으로 받아오기

--- app/src/main/java/org/seemeet/seemeet/ui/mypage/MyPageActivity.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/mypage/MyPageActivity.kt
@@ -1,10 +1,14 @@
 package org.seemeet.seemeet.ui.mypage
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import org.seemeet.seemeet.data.SeeMeetSharedPreference
 import org.seemeet.seemeet.databinding.ActivityMyPageBinding
 import org.seemeet.seemeet.ui.main.home.ChangePwActivity
@@ -31,6 +35,31 @@ class MyPageActivity : AppCompatActivity() {
         binding.etMypageId.setText(SeeMeetSharedPreference.getUserId())
     }
 
+    private val OPEN_GALLERY = 1
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.setType("image/*")
+        startActivityForResult(intent, OPEN_GALLERY)
+    }
+
+    @Override
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == OPEN_GALLERY) {
+                var currentImageUrl: Uri? = data?.data
+
+                try {
+                    Glide.with(this).load(currentImageUrl).circleCrop().into(binding.ivMypageProfile)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        } else {
+        }
+    }
+
     fun initClickListener() {
         binding.backMypage.setOnClickListener {
             finish()
@@ -54,6 +83,7 @@ class MyPageActivity : AppCompatActivity() {
 
         binding.btnSelectImage.setOnClickListener {
             //갤러리로 들어가기
+            openGallery()
         }
 
         binding.btnEditOrSave.setOnClickListener {
