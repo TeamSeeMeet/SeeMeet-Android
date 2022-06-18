74becb85071e22228bb8283d7a732e1f640e8bc1
yulim <yulim13sky@naver.com>
2022-06-18 23:22:56 +0900

    [CHORE] edittext 바깥 클릭하면 키보드 내리기

--- app/src/main/java/org/seemeet/seemeet/ui/mypage/MyPageActivity.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/mypage/MyPageActivity.kt
@@ -5,12 +5,15 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Rect
 import android.net.Uri
 import android.os.Bundle
 import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -69,9 +72,7 @@ class MyPageActivity : AppCompatActivity() {
         }
 
         viewModel.mypageName.postValue(SeeMeetSharedPreference.getUserName())
-        //binding.etMypageName.hint = SeeMeetSharedPreference.getUserName()
         viewModel.mypageId.postValue(SeeMeetSharedPreference.getUserId())
-        //binding.etMypageId.hint = SeeMeetSharedPreference.getUserId()
     }
 
     private fun statusObserver() {
@@ -109,44 +110,44 @@ class MyPageActivity : AppCompatActivity() {
         }
 
         viewModel.mypageId.observe(this) {
-                //그 직전 값이랑 입력값 비교해서 현재 커서 위치 알아내기
-                var cursor_pos=it.length
-                //입력한 경우
-                if (it.length > 0 && prev_etId!!.length < it.length) {
-                    for (i in 0..prev_etId!!.length - 1) {
-                        if (prev_etId!![i].lowercase() != it[i].lowercase()) {
-                            cursor_pos = i + 1
-                            break
-                        }
+            //그 직전 값이랑 입력값 비교해서 현재 커서 위치 알아내기
+            var cursor_pos = it.length
+            //입력한 경우
+            if (it.length > 0 && prev_etId!!.length < it.length) {
+                for (i in 0..prev_etId!!.length - 1) {
+                    if (prev_etId!![i].lowercase() != it[i].lowercase()) {
+                        cursor_pos = i + 1
+                        break
                     }
-                    if (it[cursor_pos - 1] >= 'A' && it[cursor_pos - 1] <= 'Z') {
-                        viewModel.mypageId.value = it.substring(
-                            0,
-                            cursor_pos - 1
-                        ) + it[cursor_pos - 1].lowercase() + it.substring(cursor_pos)
-                        viewModel.upperCase.value=true
-                        viewModel.cursorPos.value=cursor_pos
-                    }
-                    prev_etId = it
                 }
-                //지운 경우(드래그 전체 선택 후 입력한 경우도 포함)
-                if(it.length > 0 && prev_etId!!.length>=it.length){
-                    for (i in 0..it.length - 1) {
-                        if (it[i].toString() != prev_etId!![i].lowercase()) {
-                            cursor_pos = i + 1
-                            break
-                        }
-                    }
-                    if (it[cursor_pos - 1] >= 'A' && it[cursor_pos - 1] <= 'Z') {
-                        viewModel.mypageId.value = it.substring(
-                            0,
-                            cursor_pos - 1
-                        ) + it[cursor_pos - 1].lowercase() + it.substring(cursor_pos)
-                        viewModel.upperCase.value=true
-                        viewModel.cursorPos.value=cursor_pos
-                    }
-                    prev_etId = it
+                if (it[cursor_pos - 1] >= 'A' && it[cursor_pos - 1] <= 'Z') {
+                    viewModel.mypageId.value = it.substring(
+                        0,
+                        cursor_pos - 1
+                    ) + it[cursor_pos - 1].lowercase() + it.substring(cursor_pos)
+                    viewModel.upperCase.value = true
+                    viewModel.cursorPos.value = cursor_pos
                 }
+                prev_etId = it
+            }
+            //지운 경우(드래그 전체 선택 후 입력한 경우도 포함)
+            if (it.length > 0 && prev_etId!!.length >= it.length) {
+                for (i in 0..it.length - 1) {
+                    if (it[i].toString() != prev_etId!![i].lowercase()) {
+                        cursor_pos = i + 1
+                        break
+                    }
+                }
+                if (it[cursor_pos - 1] >= 'A' && it[cursor_pos - 1] <= 'Z') {
+                    viewModel.mypageId.value = it.substring(
+                        0,
+                        cursor_pos - 1
+                    ) + it[cursor_pos - 1].lowercase() + it.substring(cursor_pos)
+                    viewModel.upperCase.value = true
+                    viewModel.cursorPos.value = cursor_pos
+                }
+                prev_etId = it
+            }
         }
 
         viewModel.MyPageNameIdList.observe(this, Observer {
@@ -158,32 +159,6 @@ class MyPageActivity : AppCompatActivity() {
                 CustomToast.createToast(this, it)?.show()
             }
         })
-    }
-
-    //content uri를 File path로 바꿔주는 함수
-    fun getRealPathFromURI(contentUri: Uri): String? {
-        if (contentUri.path!!.startsWith("/storage")) {
-            return contentUri.path!!
-        }
-        val id = DocumentsContract.getDocumentId(contentUri).split(":")[1]
-        val columns = arrayOf(MediaStore.Files.FileColumns.DATA)
-        val selection = MediaStore.Files.FileColumns._ID + " = " + id
-        val cursor = contentResolver.query(
-            MediaStore.Files.getContentUri("external"),
-            columns,
-            selection,
-            null,
-            null
-        )
-        try {
-            val columnIndex = cursor!!.getColumnIndex(columns[0])
-            if (cursor!!.moveToFirst()) {
-                return cursor.getString(columnIndex)
-            }
-        } finally {
-            cursor!!.close()
-        }
-        return null
     }
 
     fun initClickListener() {
@@ -200,35 +175,7 @@ class MyPageActivity : AppCompatActivity() {
                 }
                 ONEDITPROFILE -> {
                     //프로필 사진 바꾸는 서버 연결
-                    val url = currentImageUrl?.toUri()
-                    //절대 경로 받아오기
-                    val file = File(getRealPathFromURI(url!!))
-                    val requestFile =
-                        RequestBody.create("multipart/form-data".toMediaTypeOrNull(), file)
-                    val body = MultipartBody.Part.createFormData("file", file.name, requestFile)
-
-                    RetrofitBuilder.mypageService.postProfile(
-                        SeeMeetSharedPreference.getToken(),
-                        body
-                    )    // body 가 Multipart.Part
-                        .enqueue(object : Callback<ResponseMyPageProfile> {
-                            override fun onFailure(
-                                call: Call<ResponseMyPageProfile>,
-                                t: Throwable
-                            ) {
-                                Log.e("error : ", t.message ?: return)
-                            }
-
-                            override fun onResponse(
-                                call: Call<ResponseMyPageProfile>,
-                                response: Response<ResponseMyPageProfile>
-                            ) {
-                                SeeMeetSharedPreference.setUserProfile(currentImageUrl)
-                                binding.btnProfileEditOrSave.text = "프로필 사진 편집"
-                                binding.btnSelectImage.visibility = View.INVISIBLE
-                                profile_position = DEFAULT
-                            }
-                        })
+                    changeProfile()
                 }
             }
         }
@@ -259,15 +206,7 @@ class MyPageActivity : AppCompatActivity() {
         }
 
         binding.btnMypageCancel.setOnClickListener {
-            binding.btnMypageCancel.visibility = View.INVISIBLE
-            binding.btnEditOrSave.text = "수정"
-            binding.etMypageName.setText(SeeMeetSharedPreference.getUserName())
-            binding.etMypageName.isEnabled = false
-            binding.mypageLine.visibility = View.INVISIBLE
-            binding.etMypageId.setText("abcdefg_")
-            binding.etMypageId.isEnabled = false
-            binding.mypageLine2.visibility = View.INVISIBLE
-            nameId_position = DEFAULT
+            BtnCancel()
         }
 
         binding.clChangepw.setOnClickListener {
@@ -318,6 +257,66 @@ class MyPageActivity : AppCompatActivity() {
         }
     }
 
+    //프로필 사진 변경하는 함수
+    private fun changeProfile() {
+        val url = currentImageUrl?.toUri()
+        //절대 경로 받아오기
+        val file = File(getRealPathFromURI(url!!))
+        val requestFile =
+            RequestBody.create("multipart/form-data".toMediaTypeOrNull(), file)
+        val body = MultipartBody.Part.createFormData("file", file.name, requestFile)
+
+        RetrofitBuilder.mypageService.postProfile(
+            SeeMeetSharedPreference.getToken(),
+            body
+        )    // body 가 Multipart.Part
+            .enqueue(object : Callback<ResponseMyPageProfile> {
+                override fun onFailure(
+                    call: Call<ResponseMyPageProfile>,
+                    t: Throwable
+                ) {
+                    Log.e("error : ", t.message ?: return)
+                }
+
+                override fun onResponse(
+                    call: Call<ResponseMyPageProfile>,
+                    response: Response<ResponseMyPageProfile>
+                ) {
+                    SeeMeetSharedPreference.setUserProfile(currentImageUrl)
+                    binding.btnProfileEditOrSave.text = "프로필 사진 편집"
+                    binding.btnSelectImage.visibility = View.INVISIBLE
+                    profile_position = DEFAULT
+                }
+            })
+    }
+
+    //content uri를 File path로 바꿔주는 함수
+    fun getRealPathFromURI(contentUri: Uri): String? {
+        if (contentUri.path!!.startsWith("/storage")) {
+            return contentUri.path!!
+        }
+        val id = DocumentsContract.getDocumentId(contentUri).split(":")[1]
+        val columns = arrayOf(MediaStore.Files.FileColumns.DATA)
+        val selection = MediaStore.Files.FileColumns._ID + " = " + id
+        val cursor = contentResolver.query(
+            MediaStore.Files.getContentUri("external"),
+            columns,
+            selection,
+            null,
+            null
+        )
+        try {
+            val columnIndex = cursor!!.getColumnIndex(columns[0])
+            if (cursor!!.moveToFirst()) {
+                return cursor.getString(columnIndex)
+            }
+        } finally {
+            cursor!!.close()
+        }
+        return null
+    }
+
+    //갤러리 들어가는 함수
     private val OPEN_GALLERY = 1
     private fun openGallery() {
         /* 권한 받기 */
@@ -336,7 +335,7 @@ class MyPageActivity : AppCompatActivity() {
         }
     }
 
-    //권한 관련
+    //권한 관련 함수
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -388,7 +387,7 @@ class MyPageActivity : AppCompatActivity() {
         binding.etMypageName.setText(SeeMeetSharedPreference.getUserName())
         binding.mypageLine.visibility = View.VISIBLE
         binding.etMypageId.isEnabled = true
-        binding.etMypageId.setText("abcdefg_")
+        binding.etMypageId.setText(SeeMeetSharedPreference.getUserId())
         binding.mypageLine2.visibility = View.VISIBLE
         nameId_position = ONEDITNAMEID
     }
@@ -404,6 +403,36 @@ class MyPageActivity : AppCompatActivity() {
         binding.etMypageId.isEnabled = false
         binding.mypageLine2.visibility = View.INVISIBLE
         nameId_position = DEFAULT
+    }
+
+    private fun BtnCancel() {
+        // 취소 버튼을 누를 경우
+        binding.btnMypageCancel.visibility = View.INVISIBLE
+        binding.btnEditOrSave.text = "수정"
+        binding.etMypageName.setText(SeeMeetSharedPreference.getUserName())
+        binding.etMypageName.isEnabled = false
+        binding.mypageLine.visibility = View.INVISIBLE
+        binding.etMypageId.setText(SeeMeetSharedPreference.getUserId())
+        binding.etMypageId.isEnabled = false
+        binding.mypageLine2.visibility = View.INVISIBLE
+        nameId_position = DEFAULT
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        val focusView = currentFocus
+        if (focusView != null && ev != null) {
+            val rect = Rect()
+            focusView.getGlobalVisibleRect(rect)
+            val x = ev.x.toInt()
+            val y = ev.y.toInt()
+
+            if (!rect.contains(x, y)) {
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm?.hideSoftInputFromWindow(focusView.windowToken, 0)
+                focusView.clearFocus()
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 
     companion object {
