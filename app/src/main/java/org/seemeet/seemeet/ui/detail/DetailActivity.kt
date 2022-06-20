e08fbfc5747d02693a0167ae48ebe6be95bb3fc9
Hyuna Kim <akxmcse@gmail.com>
2022-06-21 03:11:15 +0900

    [FIX] 취소된 약속의 상세뷰에서 약속 삭제 버튼 지우기

--- app/src/main/java/org/seemeet/seemeet/ui/detail/DetailActivity.kt
+++ app/src/main/java/org/seemeet/seemeet/ui/detail/DetailActivity.kt
@@ -3,6 +3,7 @@ package org.seemeet.seemeet.ui.detail
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -120,6 +121,8 @@ class DetailActivity : AppCompatActivity() {
                         isEnabled = false
                     })
                 }
+
+                binding.btnAppointmentCancel.visibility = View.GONE
             }
         })
 
