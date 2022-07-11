523e5b32bd0ce6c2e25e38a6f3d03749896aab89
yulim <yulim13sky@naver.com>
2022-07-11 23:54:39 +0900

    [FIX] responseChangePW 변경된 형태 수정

--- app/src/main/java/org/seemeet/seemeet/data/model/response/mypage/ResponseChangePW.kt
+++ app/src/main/java/org/seemeet/seemeet/data/model/response/mypage/ResponseChangePW.kt
@@ -3,7 +3,7 @@ package org.seemeet.seemeet.data.model.response.mypage
 import org.seemeet.seemeet.data.model.response.login.ExUser
 
 data class ResponseChangePW(
-    val data: List<ExUser>,
+    val data: ExUser,
     val status: Int,
     val success: Boolean,
     val message: String
